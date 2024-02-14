b92455b70573fb9b3922bdcfec632cce90b5fff9
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-02-14 12:12:56 -0300

    cms page content on faq all questions screen

--- src/components/cmsSections/index.js
+++ src/components/cmsSections/index.js
@@ -1,9 +1,13 @@
 import { Footer } from "../commons/Footer";
 import { Menu } from "../commons/Menu";
+import { PagefaqDisplayquestionsectionRecord } from "./PagefaqDisplayquestionsectionRecord";
 import { PagehomeHerosectionRecord } from "./PagehomeHerosectionRecord";
 import { SEOBlock } from "./SEOBlock";
 
 export const cmsSections = {
+  PagefaqDisplayquestionsectionRecord: (props) => (
+    <PagefaqDisplayquestionsectionRecord {...props} />
+  ),
   CommonSeoBlockRecord: (props) => <SEOBlock {...props} />,
   CommonMenuRecord: (props) => <Menu {...props} />,
   PagehomeHerosectionRecord: (props) => (
