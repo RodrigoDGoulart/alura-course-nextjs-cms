96b91da9485a29fb41d161c497d6495dea079d2d
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-02-14 13:17:53 -0300

    feat: visible or invisible footer

--- src/infra/cms/CMSSectionRender.js
+++ src/infra/cms/CMSSectionRender.js
@@ -5,8 +5,10 @@ export function CMSSectionRender({ pageName }) {
   const sections = getCmsContent(`${pageName}.pageContent.section`);
   return sections.map((sectionProps) => {
     const Component = cmsSections[sectionProps.componentName];
+    const isVisible =
+      sectionProps.visible === true || sectionProps.visible === undefined;
 
-    return Component ? (
+    return Component && isVisible ? (
       <Component key={sectionProps.id} {...sectionProps} />
     ) : null;
   });
